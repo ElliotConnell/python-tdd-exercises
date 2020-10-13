04e415d38b3a71fecfc4ed09dafb990cb8cf38a5
Elliot Connell <elliot.connell@gmail.com>
2020-10-14 09:43:42 +1100

    uncommented the sixth test

--- exercises.py
+++ exercises.py
@@ -116,17 +116,17 @@ def test_histogram():
 
 # # ------------------------------------------------------------------------------
 
-# def get_word_lengths(s):
-#     """
-#     Returns a list of integers representing
-#     the word lengths in string s.
-#     """
-#     return None
+def get_word_lengths(s):
+    """
+    Returns a list of integers representing
+    the word lengths in string s.
+    """
+    return None
 
 
-# def test_get_word_lengths():
-#     text = "Three tomatoes are walking down the street"
-#     assert get_word_lengths(text) == [5, 8, 3, 7, 4, 3, 6]
+def test_get_word_lengths():
+    text = "Three tomatoes are walking down the street"
+    assert get_word_lengths(text) == [5, 8, 3, 7, 4, 3, 6]
 
 
 # # ------------------------------------------------------------------------------
